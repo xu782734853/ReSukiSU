f593e7bc1f2ba000e87f7b77494664efe09a6fa9
Huy Minh <39849246+hmtheboy154@users.noreply.github.com>
2026-03-09 22:25:10 +0700

    kernel: Only check CONFIG_STACKPROTECTOR_PER_TASK on arm64 (https://github.com/tiann/KernelSU/pull/3263)
    CONFIG_STACKPROTECTOR_PER_TASK does not exist on x86_64. This will fix
    building KSU on that arch.
    
    Fixes:
    https://github.com/tiann/KernelSU/commit/baf3f7d4675b42e297262cbe1cd1ecbede5e34e5
    
    [cherry-picked from upstream commit https://github.com/tiann/KernelSU/commit/2c1d81585f10746757c33b325e61a82db3f9de80]
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>
    Signed-off-by: AlexLiuDev233 <wzylin11@outlook.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -30,7 +30,8 @@
 // Therefore, ksu lkm, which uses gki toolchain, requires this __stack_chk_guard,
 // while those third-party kernel can't provide.
 // Thus, we manually provide it instead of using kernel's
-#if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
+#if defined(CONFIG_STACKPROTECTOR) &&                                          \
+    (defined(CONFIG_ARM64) && !defined(CONFIG_STACKPROTECTOR_PER_TASK))
 #include <linux/stackprotector.h>
 #include <linux/random.h>
 unsigned long __stack_chk_guard __ro_after_init
@@ -68,7 +69,8 @@ int __init kernelsu_init(void)
     pr_info("Initialized on: %s (%s) with driver version: %u\n", UTS_RELEASE,
             UTS_MACHINE, KSU_VERSION);
 #ifdef MODULE
-#if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
+#if defined(CONFIG_STACKPROTECTOR) &&                                          \
+    (defined(CONFIG_ARM64) && !defined(CONFIG_STACKPROTECTOR_PER_TASK))
     unsigned long canary;
 
     /* Try to get a semi random initial value. */
